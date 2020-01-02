d2ad3a9f00c5d2564758075235ec1a7d9d937733
Lucas Jardim <lcsjardim@gmail.com>
2020-01-02 15:09:52 -0300

    post-item component

---
+++ src/components/Posts/post-item.vue
@@ -0,0 +1,38 @@
+<template>
+    <article class="media is-clickable" @click="$emit('show-details',post)">
+        <div class="media-content">
+            <div class="content">
+                <p>
+                    <strong>#{{post.id}} - {{post.title}}</strong>
+                    <br>
+                    {{post.body|limit}}
+                </p>
+            </div>
+        </div>
+        <div class="media-right">
+            <a href="">Edit</a>
+        </div>
+    </article>
+</template>
+
+<script>
+    export default {
+        props: {
+            post: {
+                type: Object,
+                default: ()=>{},
+            }
+        },
+        filters: {
+            limit: function (text) {
+                return text.substr(0,50) + ' ...'
+            }
+        }
+    }
+</script>
+
+<style>
+    .is-clickable{
+        cursor: pointer;
+    }
+</style>
