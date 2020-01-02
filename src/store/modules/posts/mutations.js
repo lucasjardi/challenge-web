e9d4122c8cd73c2ddecb9bc5531777782935b887
Lucas Jardim <lcsjardim@gmail.com>
2020-01-02 19:34:44 -0300

    new mutation to update a post from the post list

--- src/store/modules/posts/mutations.js
+++ src/store/modules/posts/mutations.js
@@ -4,4 +4,8 @@ export default {
     [ types.POSTS ] (state, posts) {
         state.posts = posts
     },
+    [ types.UPDATE_POST ] (state, payload) {
+        let index = state.posts.findIndex(post=>post.id === payload.id)
+        state.posts.splice(index,1,payload)
+    },
 }
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,2 +1,3 @@
 export const POSTS = 'POSTS'
 export const USER = 'USER'
+export const UPDATE_POST = 'UPDATE_POST'
