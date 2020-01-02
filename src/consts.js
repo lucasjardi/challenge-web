6ddf0f83d5d9fd5cd17a35cc0d9413541edef1fb
Lucas Jardim <lcsjardim@gmail.com>
2020-01-02 14:43:37 -0300

    consts.js

---
+++ src/consts.js
@@ -0,0 +1,20 @@
+const api_endpoint = 'http://jsonplaceholder.typicode.com'
+const show_modal_code = 'show-modal-post-details'
+const post = {
+    userId: '',
+    id: '',
+    title: '',
+    body: '',
+}
+const user = {
+    name: '',
+    username: '',
+    email: '',
+    address: {},
+    phone: '',
+    company: {
+        name: ''
+    },
+}
+
+export {api_endpoint, show_modal_code, post, user}
