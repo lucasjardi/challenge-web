8c50e387f8e115f0762ad65169e7e40a82e399bf
Lucas Jardim <lcsjardim@gmail.com>
2020-01-02 19:33:14 -0300

    modal-post-edit

---
+++ src/components/Posts/modal-post-edit.vue
@@ -0,0 +1,54 @@
+<template>
+    <modal-card v-if="visible" @close="close" @save="save">
+        <template slot="title">Details</template>
+        <template slot="body">
+            <div class="field">
+                <label class="label">Title</label>
+                <div class="control">
+                    <input class="input" v-model="post.title" type="text" placeholder="Title input">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Body</label>
+                <div class="control">
+                    <textarea v-model="post.body" class="textarea" placeholder="Body..."></textarea>
+                </div>
+            </div>
+        </template>
+    </modal-card>
+</template>
+
+<script>
+    import ModalCard from './../Util/modal-card'
+    import * as consts from '../../consts'
+    import * as mutations from '../../store/mutation-types'
+
+    export default {
+        components: {
+            ModalCard,
+        },
+        data() {
+            return {
+                visible: false,
+                post: consts.POST_DEFAULT,
+            }
+        },
+        mounted(){
+            this.$parent.$on(consts.SHOW_MODAL_EDIT, post=>{
+                this.visible = true
+                this.post = JSON.parse(JSON.stringify(post))
+            })
+        },
+        methods: {
+            save(){
+                if(!this.post.title || !this.post.body) return
+                this.$store.commit(mutations.UPDATE_POST, this.post)
+                this.visible = false
+            },
+            close(){
+                this.visible = false
+                this.post = consts.POST_DEFAULT
+            }
+        },
+    }
+</script>
