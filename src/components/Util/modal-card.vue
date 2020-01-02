66b9ad37052a5ff156ec472aec2321f87daf1623
Lucas Jardim <lcsjardim@gmail.com>
2020-01-02 14:44:02 -0300

    modal-card base component

---
+++ src/components/Util/modal-card.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="modal is-active">
+        <div class="modal-background" @click="$emit('close')"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title is-marginless-b"><slot name="title"></slot></p>
+                <button type="button" class="delete" @click="$emit('close')"></button>
+            </header>
+            <section class="modal-card-body">
+                <slot name="body"></slot>
+            </section>
+            <footer class="modal-card-foot footer-right">
+
+                <slot name="custom"></slot>
+                <slot name="footer">
+                    <a class="button is-pulled-right" v-if="!hideCanceled" @click="$emit('close')">Cancel</a>
+                    <a class="button is-primary is-pulled-right" @click="$emit('save')">Save</a>
+                </slot>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            hideCanceled: {
+                type: Boolean,
+                default: false
+            }
+        },
+    }
+
+</script>
+
+<style>
+    .footer-right{
+        justify-content: flex-end;
+    }
+</style>
