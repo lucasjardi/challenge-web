646d87deaac620a796a656cabce4032d2065ff75
Lucas Jardim <lcsjardim@gmail.com>
2020-01-02 15:10:37 -0300

    user-details component

---
+++ src/components/Users/user-details.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>User: {{user.name}}</p>
+        <p>Username: {{user.username}}</p>
+        <p>E-mail: {{user.email}}</p>
+        <p>Address: {{user.address|toAddress}}</p>
+        <p>Phone: {{user.phone}}</p>
+        <p>Company {{user.company.name}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            user: {
+                type: Object,
+                default: ()=>{}
+            }
+        },
+        filters: {
+            toAddress: function (address) {
+                return address.street + ' - ' + address.suite + ', ' + address.city
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
